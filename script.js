7121c52a897c33767ccf6f3bcc141b29895d4435
Sugam <sugamshakya@lftechnology.com>
2016-12-26 17:32:21 +0545

    ball disappear when collide

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 	var context = canvas.getContext('2d');
 	var canvasWidth = canvas.width;
 	var canvasHeight = canvas.height;
+	var circles = [];
 
-	var Circle = function(centerX, centerY, radius){
-		this.center = {x: centerX, y: centerY};
+	var Circle = function(center, radius){
+		this.center = center;
 		this.radius = radius;
 		this.incrementX = true;
 		this.incrementY = false;
+		this.interval;
 	}
 
 	Circle.prototype.drawCircle = function(){
-		console.log('circle created')
 		context.beginPath();
 		context.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI, false);
 		context.fillStyle = 'green';
@@ -23,7 +24,7 @@
 
 	Circle.prototype.removeCircle = function(){
 		context.beginPath();
-		context.arc(this.center.x, this.center.y, this.radius*2, 0, 2 * Math.PI, false);
+		context.arc(this.center.x, this.center.y, this.radius+1, 0, 2 * Math.PI, false);
 		context.fillStyle = 'white';
 		context.fill();
 		context.lineWidth = 3;
@@ -60,21 +61,54 @@
 		}
 	};
 
-	Circle.prototype.moveCircle = function(){
-		this.removeCircle();
-		this.detectCollision();
-		this.calculateNewCenter()
-		this.drawCircle();	
+	Circle.prototype.moveCircle = function(){	
+		var that = this;
+		this.interval = setInterval(function(){
+			that.removeCircle();
+			that.detectCollision();
+			that.calculateNewCenter()
+			that.drawCircle();
+			that.circleIntersection();
+		}, 15);
 	};
 
-	centerX = Math.floor(Math.random()*canvasWidth);
-	centerY = Math.floor(Math.random()*canvasHeight);
+	Circle.prototype.circleIntersection = function(){
+		var currentCircleContext = this;
+		circles.forEach( function(element, index) {
+			if( element != currentCircleContext){
+				// check if the circle touch any other circle
+				var distance = Math.pow((currentCircleContext.radius + element.radius), 2);
+				var calculatedDistance = Math.pow((element.center.x - currentCircleContext.center.x), 2) 
+				+ Math.pow((element.center.y - currentCircleContext.center.y), 2);
+				if(calculatedDistance <= distance){
+					currentCircleContext.removeCircle();
+					element.removeCircle();
+					circles.splice(circles.indexOf(currentCircleContext), 1);
+					circles.splice(circles.indexOf(element), 1);
+					clearInterval(currentCircleContext.interval);
+					clearInterval(element.interval);
+				}
+			}
+		});
+	}
+
+	Circle.prototype.start = function(){
+		this.drawCircle();
+		this.moveCircle();
+		circles.push(this);
+	};
+
+	function randomCenter(){
+		return {
+			x: Math.floor(Math.random()*canvasWidth),
+			y: Math.floor(Math.random()*canvasHeight)
+		}
+	}
+
 	radius = 15;
-
-	var circle = new Circle(centerX, centerY, radius);
-	circle.drawCircle();
-
-	var c = new Circle(0,canvasHeight,5);
-	c.drawCircle();
-
-	setInterval(function(){ circle.moveCircle(); }, 15);
+	c = []
+	for (var i = 0; i < 5; i++) {
+		c[i] = new Circle(randomCenter(), radius);
+		c[i].start();
+	}
+	
